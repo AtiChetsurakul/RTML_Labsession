af5f11fbbe12f956fd239c70b8a05a97eee099a9
ati <ksde34@gmail.com>
2023-02-28 13:10:41 +0000

    updaste

---
+++ 06_wesersteinGGG/cdUtilGan.py
@@ -0,0 +1,194 @@
+import torch.nn.functional as F
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0)  # Set for our testing purposes, please do not change!
+
+
+def get_input_dimensions(z_dim, mnist_shape, n_classes):
+    '''
+    Function for getting the size of the conditional input dimensions 
+    from z_dim, the image shape, and number of classes.
+    Parameters:
+        z_dim: the dimension of the noise vector, a scalar
+        mnist_shape: the shape of each MNIST image as (C, W, H), which is (1, 28, 28)
+        n_classes: the total number of classes in the dataset, an integer scalar
+                (10 for MNIST)
+    Returns: 
+        generator_input_dim: the input dimensionality of the conditional generator, 
+                          which takes the noise and class vectors
+        discriminator_im_chan: the number of input channels to the discriminator
+                            (e.g. C x 28 x 28 for MNIST)
+    '''
+    generator_input_dim = z_dim + n_classes
+    discriminator_im_chan = mnist_shape[0] + n_classes
+    return generator_input_dim, discriminator_im_chan
+
+
+def combine_vectors(x, y):
+    '''
+    Function for combining two vectors with shapes (n_samples, ?) and (n_samples, ?).
+    Parameters:
+      x: (n_samples, ?) the first vector. 
+        In this assignment, this will be the noise vector of shape (n_samples, z_dim), 
+        but you shouldn't need to know the second dimension's size.
+      y: (n_samples, ?) the second vector.
+        Once again, in this assignment this will be the one-hot class vector 
+        with the shape (n_samples, n_classes), but you shouldn't assume this in your code.
+    '''
+    # Note: Make sure this function outputs a float no matter what inputs it receives
+    combined = torch.cat((x.float(), y.float()), dim=1)
+    return combined
+
+
+def get_one_hot_labels(labels, n_classes):
+    '''
+    Function for creating one-hot vectors for the labels, returns a tensor of shape (?, num_classes).
+    Parameters:
+        labels: tensor of labels from the dataloader, size (?)
+        n_classes: the total number of classes in the dataset, an integer scalar
+    '''
+    return F.one_hot(labels, n_classes)
+
+
+class Discriminator(nn.Module):
+    '''
+    Discriminator Class
+    Values:
+      im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+            (MNIST is black-and-white, so 1 channel is your default)
+      hidden_dim: the inner dimension, a scalar
+    '''
+
+    def __init__(self, im_chan=1, hidden_dim=64):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self.make_disc_block(im_chan, hidden_dim),
+            self.make_disc_block(hidden_dim, hidden_dim * 2),
+            self.make_disc_block(hidden_dim * 2, 1, final_layer=True),
+        )
+
+    def make_disc_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a discriminator block of the DCGAN; 
+        a convolution, a batchnorm (except in the final layer), and an activation (except in the final layer).
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels,
+                          kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels,
+                          kernel_size, stride),
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the discriminator: Given an image tensor, 
+        returns a 1-dimension tensor representing fake/real.
+        Parameters:
+            image: a flattened image tensor with dimension (im_chan)
+        '''
+        disc_pred = self.disc(image)
+        return disc_pred.view(len(disc_pred), -1)
+
+
+class Generator(nn.Module):
+    '''
+    Generator Class
+    Values:
+        input_dim: the dimension of the input vector, a scalar
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (MNIST is black-and-white, so 1 channel is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+
+    def __init__(self, input_dim=10, im_chan=1, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.input_dim = input_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(input_dim, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim *
+                                2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan,
+                                kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN;
+        a transposed convolution, a batchnorm (except in the final layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(
+                    input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(
+                    input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, input_dim)
+        '''
+        x = noise.view(len(noise), self.input_dim, 1, 1)
+        return self.gen(x)
+
+
+def get_noise(n_samples, input_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, input_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        input_dim: the dimension of the input vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, input_dim, device=device)
+
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), nrow=5, show=True):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    if show:
+        plt.show()
